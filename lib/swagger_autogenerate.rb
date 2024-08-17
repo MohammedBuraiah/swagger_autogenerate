9586315548ab1de6ebb0eea222388551ed782622
mohammed-buraiah <mohammed.buraiah@jisr.net>
2024-08-17 04:13:26 +0300

    add rspec_examples

--- lib/swagger_autogenerate.rb
+++ lib/swagger_autogenerate.rb
@@ -8,9 +8,23 @@ module SwaggerAutogenerate
     def process_action(*args)
       super
 
-      if ENV[SwaggerTrace.swagger_environment_variable].present? && Rails.env.send("#{SwaggerTrace.environment_name.to_s}?")
+      if SwaggerAutogenerate.allow_swagger?
         SwaggerTrace.new(request, response).call
       end
+    end
+  end
+
+  def self.allow_swagger?
+    ENV[SwaggerTrace.swagger_environment_variable].present? && Rails.env.send("#{SwaggerTrace.environment_name.to_s}?")
+  end
+end
+
+if defined?(RSpec) && SwaggerAutogenerate.allow_swagger?
+  require 'rspec/rails'
+
+  RSpec.configure do |config|
+    config.before(:each) do |example|
+      SwaggerAutogenerate::SwaggerTrace.rspec_description = SwaggerAutogenerate.extract_description(example.metadata[:full_description])
     end
   end
 end
