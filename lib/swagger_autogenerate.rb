8970ec1b9e0a967e0ec11311343ca375b0dd8022
MohammedBuraiah <mohammed.buraiah.1996@gmail.com>
2025-07-17 09:14:28 +0300

    version 1.2.8

--- lib/swagger_autogenerate.rb
+++ lib/swagger_autogenerate.rb
@@ -12,20 +12,20 @@ module SwaggerAutogenerate
         SwaggerTrace.new(request, response).call
       end
     end
+
+    if defined?(RSpec) && SwaggerAutogenerate.allow_swagger?
+      require 'rspec/rails'
+
+      RSpec.configure do |config|
+        config.before(:each) do |example|
+          SwaggerAutogenerate::SwaggerTrace.rspec_description = example&.metadata.dig(:example_group, :description)
+        end
+      end
+    end
   end
 
   def self.allow_swagger?
     (ENV[SwaggerTrace.swagger_path_environment_variable].present? || ENV[SwaggerTrace.generate_swagger_environment_variable].present?) &&
     Rails.env.send("#{SwaggerTrace.environment_name.to_s}?")
-  end
-end
-
-if defined?(RSpec) && SwaggerAutogenerate.allow_swagger?
-  require 'rspec/rails'
-
-  RSpec.configure do |config|
-    config.before(:each) do |example|
-      SwaggerAutogenerate::SwaggerTrace.rspec_description = example&.metadata.dig(:example_group, :description)
-    end
   end
 end
